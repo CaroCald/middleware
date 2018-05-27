58fa2a4c5c4f2a7ef1682d7e9de8638a1a7d44a4
CaroCald <carolina.2594@hotmail.com>
2018-05-26 23:55:45 -0500

    segunda logica

--- project/src/usuario.controller.ts
+++ project/src/usuario.controller.ts
@@ -4,15 +4,10 @@ import {Controller, Get, Req, Res} from "@nestjs/common";
 export class UsuarioController {
 
     @Get('sesion')
-    sesion(@Res() response, @Req() request){
-        const parametros={
-            nombre: 'EN CACHE',
-            valor: 'CACHE'
-        };
-        console.log(parametros.nombre, parametros.valor);
-        response.cookie(parametros.nombre, parametros.valor);
-        return response.send();
+    ponerCookie(@Res() response, @Req() request){
+
     }
+
 
 
 }
--- project/src/usuario.middleware.ts
+++ project/src/usuario.middleware.ts
@@ -11,8 +11,32 @@ export class usuarioMiddleware implements NestMiddleware {
             {
                 response.send({mensaje: 'No esta en cache'});
             }*/
+            this.comparar(response, request);
             next();
 
         }
+
         }
+
+    ponerCookie(response, parametro:string){
+        console.log(parametro);
+        response.cookie(parametro);
+        return response.send();
+    }
+    getCookie(request){
+        console.log(request.cookies);
+        return request.cookies;
+    }
+    comparar(response, request){
+       const variable =  this.getCookie(request);
+       if(variable!=undefined){
+        response.send({mensaje: 'SI ESTA EN CACHE'});
+       }else
+       {
+           response.send({mensaje: 'NO ESTA EN CACHE'});
+           if(variable==undefined){
+               this.ponerCookie(response, 'en cache');
+           }
+       }
+    }
 }
